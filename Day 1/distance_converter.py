2d95b84a5480d288f8751341bee5b85816479af3
Krishna Gelra <krishnagelra18@gmail.com>
2025-11-26 23:04:00 +0530

    Day 1 files modified

--- Day 1/distance_converter.py
+++ Day 1/distance_converter.py
@@ -1,17 +1,16 @@
 # Convert distance from Miles to Kilometers or Kilometers to Miles
 
 # Constants
-CONVERSION_RATE = 1.609
+CONVERSION_RATE : float = 1.609
 
 # User Inputs
-conversion_type = int(input("Enter 1 to convert miles to kilometers or enter 2 to convert kilometers to miles: "))
-distance_input = int(input("Enter to distance value: "))
+conversion_type : int = int(input("Enter 1 to convert miles to kilometers or enter 2 to convert kilometers to miles: "))
+distance_input : float = float(input("Enter to distance value: "))
 
 # Calculation and Output
-match conversion_type:
-    case 1:
-        distance_output = distance_input * CONVERSION_RATE
-        print(f"\nConversion: {distance_input} Miles -> {distance_output:.2f} Kilometers")
-    case 2:
-        distance_output = distance_input / CONVERSION_RATE
-        print(f"\nConversion: {distance_input} Kilometers -> {distance_output:.2f} Miles")
+if conversion_type == 1:
+    distance_output : float = distance_input * CONVERSION_RATE
+    print(f"\nConversion: {distance_input} Miles -> {distance_output:.2f} Kilometers")
+else:
+    distance_output : float = distance_input / CONVERSION_RATE
+    print(f"\nConversion: {distance_input} Kilometers -> {distance_output:.2f} Miles")
