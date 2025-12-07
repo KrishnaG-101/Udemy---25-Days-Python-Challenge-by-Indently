1ef773c145fe5ed6d0e6e4cddf6c2b178249b539
Krishna Gelra <krishnagelra18@gmail.com>
2025-12-07 13:42:48 +0530

    text_analyzer.py modified -> v2

--- Day 19/text_analyzer.py
+++ Day 19/text_analyzer.py
@@ -8,33 +8,39 @@ def analyze(path : str) -> None:
         text : str = file.read()
     
     # list of words
-    words : list[str] = re.findall(r"\b\w+\b", text.lower())
-    
-    # count of white spaces
-    white_space_count : int = sum(char.isspace() for char in text)
-    # count of characters
-    character_count_with_space : int = len(text)
+    words : list[str] = re.findall(r"\b\w+\b", text.lower()) 
     # count of words
     word_count : int = len(words)
-    
-    # count of each punctuation [",", ".", ";", ":", "'", "-", "!"]
-    
+    # count of characters
+    char_count_with_space : int = len(text)
+    # count of white space and punctuation
+    # we haven't applied .strip() to text so spaces before and after will be counted too.
+    char_counter : Counter[str] = Counter(text)
+    # count of white spaces
+    # sum(c.isspace for c in text) -> 136 as it counts "\n" too, but text.count(" ") -> 118 as count of white spaces.
+    white_space_count : int = char_counter[" "]
     # 3 most common words
     three_most_common_words : list[tuple[str, int]] = Counter(words).most_common(3)
     
     # display
     print("-" * 30)
-    print(f"Number of characters in the text: {character_count_with_space}")
+    print(f"Number of characters in the text: {char_count_with_space}")
     print(f"Number of white spaces in the text: {white_space_count}")
     print(f"Number of words in the text: {word_count}")
+    
+    # count of each punctuation [",", ".", ";", ":", "'", "-", "!"]
+    print("\nPunctuation count:")
+    for punctuation in [",", ".", "-", ";", ":", "'", "!"]:
+        print(f"=> {punctuation} : {char_counter[punctuation]}")
+    
     print("\nThree most common words in our text:")
     # Well I am a bit confused on how the code below works absolutely correctly.
     # Because the three_most_common_words contains tuples inside which we have the values
     # I mean it should be for tuple_ in variable: and we access "tuple_[0]" for word and "tuple_[1]" for frequency. right?
     for word, frequency in three_most_common_words:
-        print(f"> {word}: {frequency}")
+        print(f"=> {word} : {frequency}")
     print("-" * 30)
-    
+
 def main() -> None:
     analyze("Day 19/sample.txt")
 
